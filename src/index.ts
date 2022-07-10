428e039d8ffc422d12b2ca95a289db409893a33a
takuabonn <k.takumi19980322@gmail.com>
2022-07-10 17:07:25 +0900

    数字パネル１マス移動を一回する時の最大数を求める_マジックナンバーじゃなくてインスタンス変数を使うように修正

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+type numberList = number[];
+type numberPanel = numberList[];
+
 export class NumberBox {
-  public maxResultNumberWithMoving: number;
-  constructor() {
+  private maxResultNumberWithMoving: number;
+  public numberPanel: numberPanel;
+  constructor(number_panel: numberPanel) {
     this.maxResultNumberWithMoving = 9;
+    this.numberPanel = number_panel;
+  }
+
+  getMaxResultNumberWithMoving() {
+    return this.maxResultNumberWithMoving;
   }
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,7 @@ import { NumberBox } from "../src/index";
 
 describe("number box", () => {
   describe("特定の数字パネルを１マス移動を n-1 回繰り返した時の最大数", () => {
-    const A = [
+    const numberPanel = [
       [1, 1, 6, 1],
       [1, 1, 1, 9],
       [7, 1, 1, 1],
@@ -10,9 +10,10 @@ describe("number box", () => {
     ];
 
     test("数字パネル１マス移動を一回する時の最大数を求める", () => {
-      const numberBox = new NumberBox();
-      const maxResultNumberWithMoving = numberBox.maxResultNumberWithMoving;
+      const numberBox = new NumberBox(numberPanel);
 
+      const maxResultNumberWithMoving =
+        numberBox.getMaxResultNumberWithMoving();
       expect(maxResultNumberWithMoving).toBe(9);
     });
   });
