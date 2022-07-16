1f83981a3ac009f94c600940d45651b86bebbfd2
takuabonn <k.takumi19980322@gmail.com>
2022-07-16 18:40:36 +0900

    最大数の位置(i,j)の配列を返す

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 type numberList = number[];
 type numberPanel = numberList[];
+interface MaxNumberPositionArr {
+  number: number;
+  i: number;
+  j: number;
+}
 export const maxNumber = (numberPanel: numberPanel) => {
-  let max_number = 0;
+  let max_number = Math.max(...numberPanel.flat());
+  const maxNumberPositionArr = [];
   for (let i = 0; i < numberPanel.length; i++) {
     for (let j = 0; j < numberPanel[i].length; j++) {
       const curent_number = numberPanel[i][j];
-      if (curent_number < max_number) {
+      if (curent_number !== max_number) {
         continue;
       }
-      max_number = curent_number;
+      maxNumberPositionArr.push([i, j]);
     }
   }
-  return max_number;
+  return maxNumberPositionArr;
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,16 @@
 import { maxNumber } from "../src/index";
 
 describe("number box", () => {
-  test("数字パネルの最大数を求める", () => {
+  test("数字パネルの最大数が複数ある時最大数の位置(i,j)の配列を返す", () => {
     const numberPanel = [
-      [1, 1, 6, 1],
+      [1, 9, 6, 1],
       [1, 1, 1, 9],
       [7, 1, 1, 1],
       [1, 8, 1, 1],
     ];
-    expect(maxNumber(numberPanel)).toBe(9);
+    const max_number_arr = maxNumber(numberPanel);
+    expect(max_number_arr.length).toBe(2);
+    expect(max_number_arr[0]).toEqual(expect.arrayContaining([0, 1]));
+    expect(max_number_arr[1]).toEqual(expect.arrayContaining([1, 3]));
   });
 });
