3cec9d45c1025e7846178f95666a5b3f50ea194f
takuabonn <k.takumi19980322@gmail.com>
2022-07-16 18:04:11 +0900

    リファクタ

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 type numberList = number[];
 type numberPanel = numberList[];
 export const maxNumber = (numberPanel: numberPanel) => {
-  return 9;
+  let max_number = 0;
+  for (let i = 0; i < numberPanel.length; i++) {
+    for (let j = 0; j < numberPanel[i].length; j++) {
+      const curent_number = numberPanel[i][j];
+      if (curent_number < max_number) {
+        continue;
+      }
+      max_number = curent_number;
+    }
+  }
+  return max_number;
 };
