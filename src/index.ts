e80c89365888c23fdc19a1421fae781209962892
takuabonn <k.takumi19980322@gmail.com>
2022-07-16 20:10:17 +0900

    初回の最大数の位置から数字パネルを１回移動する時に通るマスを順番に並べた時の最大数を返す

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 type numberList = number[];
 type numberPanel = numberList[];
-interface MaxNumberPositionArr {
-  number: number;
-  i: number;
-  j: number;
-}
-export const maxNumber = (numberPanel: numberPanel) => {
+type positionArr = number[][];
+
+export const maxNumberPositionArr = (numberPanel: numberPanel) => {
   let max_number = Math.max(...numberPanel.flat());
   const maxNumberPositionArr = [];
   for (let i = 0; i < numberPanel.length; i++) {
@@ -18,4 +15,53 @@ export const maxNumber = (numberPanel: numberPanel) => {
     }
   }
   return maxNumberPositionArr;
+};
+
+const positionChangeDirectionPatterns = [
+  [1, 0],
+  [-1, 0],
+  [0, 1],
+  [0, -1],
+  [1, 1],
+  [-1, 1],
+  [-1, -1],
+  [1, -1],
+];
+
+const canMove = (i: number, j: number) => {
+  let canMove = true;
+  if (i < 0 && j > 4 - 1) canMove = false;
+  if (i > 4 - 1 && j > 4 - 1) canMove = false;
+  if (i < 0 && j < 0) canMove = false;
+  if (i > 4 - 1 && j < 0) canMove = false;
+  return canMove;
+};
+
+const indexFunc = (index: number) => {
+  if (index > 4 - 1) return 0;
+  if (index < 0) return 4 - 1;
+  return index;
+};
+
+export const maxNumber = (
+  numberPanel: numberPanel,
+  maxNumberPositionArr: positionArr,
+  s: string
+) => {
+  const numberArr: number[] = [];
+  maxNumberPositionArr.forEach((position) => {
+    const i = position[0];
+    const j = position[1];
+    for (const positionChangeDirection of positionChangeDirectionPatterns) {
+      let rowIndex = i + positionChangeDirection[0];
+      let colIndex = j + positionChangeDirection[1];
+      if (!canMove(rowIndex, colIndex)) {
+        continue;
+      }
+      const plusedS =
+        s + String(numberPanel[indexFunc(rowIndex)][indexFunc(colIndex)]);
+      numberArr.push(Number(plusedS));
+    }
+  });
+  return Math.max(...numberArr);
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { maxNumber } from "../src/index";
+import { maxNumberPositionArr, maxNumber } from "../src/index";
 
 describe("number box", () => {
   test("数字パネルの最大数が複数ある時最大数の位置(i,j)の配列を返す", () => {
@@ -8,9 +8,24 @@ describe("number box", () => {
       [7, 1, 1, 1],
       [1, 8, 1, 1],
     ];
-    const max_number_arr = maxNumber(numberPanel);
-    expect(max_number_arr.length).toBe(2);
-    expect(max_number_arr[0]).toEqual(expect.arrayContaining([0, 1]));
-    expect(max_number_arr[1]).toEqual(expect.arrayContaining([1, 3]));
+    const max_number_positionArr = maxNumberPositionArr(numberPanel);
+    expect(max_number_positionArr.length).toBe(2);
+    expect(max_number_positionArr[0]).toEqual(expect.arrayContaining([0, 1]));
+    expect(max_number_positionArr[1]).toEqual(expect.arrayContaining([1, 3]));
+  });
+  describe("初回の最大数の位置から数字パネルを１回移動する時に通るマスを順番に並べた時の最大数を返す", () => {
+    test("数字パネルを１回移動する時に通るマスを順番に並べた時の数を返す", () => {
+      const numberPanel = [
+        [1, 9, 6, 1],
+        [1, 1, 1, 9],
+        [7, 1, 1, 1],
+        [1, 8, 1, 1],
+      ];
+      const max_number_positionArr = [
+        [0, 1],
+        [1, 3],
+      ];
+      expect(maxNumber(numberPanel, max_number_positionArr, "9")).toBe(98);
+    });
   });
 });
