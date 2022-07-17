7c7b0c115b204af6364d386ae3ea2c88e02f0c8c
takuabonn <k.takumi19980322@gmail.com>
2022-07-17 17:55:33 +0900

    nを変数に変更

--- src/index.ts
+++ src/index.ts
@@ -29,19 +29,19 @@ const positionChangeDirectionPatterns = [
   [-1, -1],
   [1, -1],
 ];
-
+let iniN = 0;
 const canMove = (i: number, j: number) => {
   let canMove = true;
-  if (i < 0 && j > 4 - 1) canMove = false;
-  if (i > 4 - 1 && j > 4 - 1) canMove = false;
+  if (i < 0 && j > iniN - 1) canMove = false;
+  if (i > iniN - 1 && j > iniN - 1) canMove = false;
   if (i < 0 && j < 0) canMove = false;
-  if (i > 4 - 1 && j < 0) canMove = false;
+  if (i > iniN - 1 && j < 0) canMove = false;
   return canMove;
 };
 
 const indexFunc = (index: number) => {
-  if (index > 4 - 1) return 0;
-  if (index < 0) return 4 - 1;
+  if (index > iniN - 1) return 0;
+  if (index < 0) return iniN - 1;
   return index;
 };
 
@@ -57,7 +57,6 @@ const roopFunc = (
   const i = beforeRowIndex;
   const j = beforeColIndex;
   let s = re_s;
-  // const numberArr: number[] = [];
   for (const [
     index,
     positionChangeDirection,
@@ -103,82 +102,12 @@ export const maxNumber = (
   maxNumberPositionArr: positionArr,
   s: string
 ) => {
-  // const numberArr: number[] = [];
+  iniN = n;
+
   maxNumberPositionArr.forEach((position) => {
     const i = position[0];
     const j = position[1];
-    roopFunc(n, numberPanel, i, j, s, [[i, j]]);
-
-    // for (const positionChangeDirection of positionChangeDirectionPatterns) {
-    //   const arr: number[][] = [];
-    //   arr.push([i, j]);
-    //   if (
-    //     !canMove(i + positionChangeDirection[0], j + positionChangeDirection[1])
-    //   ) {
-    //     continue;
-    //   }
-    //   let rowIndex = indexFunc(i + positionChangeDirection[0]);
-    //   let colIndex = indexFunc(j + positionChangeDirection[1]);
-    //   if (
-    //     arr.filter((value) => value[0] === rowIndex && value[1] === colIndex)
-    //       .length
-    //   ) {
-    //     continue;
-    //   }
-    //   arr.push([rowIndex, colIndex]);
-    //   const plusedS =
-    //     s + String(numberPanel[indexFunc(rowIndex)][indexFunc(colIndex)]);
-
-    //   for (const positionChangeDirection of positionChangeDirectionPatterns) {
-    //     const arr2: number[][] = [...arr];
-    //     if (
-    //       !canMove(
-    //         rowIndex + positionChangeDirection[0],
-    //         colIndex + positionChangeDirection[1]
-    //       )
-    //     ) {
-    //       continue;
-    //     }
-    //     let rowIndex2 = indexFunc(rowIndex + positionChangeDirection[0]);
-    //     let colIndex2 = indexFunc(colIndex + positionChangeDirection[1]);
-    //     if (
-    //       arr2.filter(
-    //         (value) => value[0] === rowIndex2 && value[1] === colIndex2
-    //       ).length
-    //     ) {
-    //       continue;
-    //     }
-    //     arr2.push([rowIndex2, colIndex2]);
-
-    //     const plusedS2 = plusedS + String(numberPanel[rowIndex2][colIndex2]);
-
-    //     for (const positionChangeDirection of positionChangeDirectionPatterns) {
-    //       const arr3: number[][] = [...arr2];
-    //       if (
-    //         !canMove(
-    //           rowIndex2 + positionChangeDirection[0],
-    //           colIndex2 + positionChangeDirection[1]
-    //         )
-    //       ) {
-    //         continue;
-    //       }
-    //       let rowIndex3 = indexFunc(rowIndex2 + positionChangeDirection[0]);
-    //       let colIndex3 = indexFunc(colIndex2 + positionChangeDirection[1]);
-    //       if (
-    //         arr3.filter(
-    //           (value) => value[0] === rowIndex3 && value[1] === colIndex3
-    //         ).length
-    //       ) {
-    //         continue;
-    //       }
-    //       arr3.push([rowIndex3, colIndex3]);
-
-    //       const plusedS3 = plusedS2 + String(numberPanel[rowIndex3][colIndex3]);
-
-    //       numberArr.push(Number(plusedS3));
-    //     }
-    //   }
-    // }
+    roopFunc(iniN - 1, numberPanel, i, j, s, [[i, j]]);
   });
   return Math.max(...numberArr);
 };
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -15,16 +15,11 @@ describe("number box", () => {
   });
   test("初回の最大数の位置かsら数字パネルを1回移動する時に通るマスを順番に並べた時の最大数を返す", () => {
     const numberPanel = [
-      [1, 9, 6, 1],
-      [1, 1, 1, 9],
-      [7, 1, 1, 1],
-      [1, 8, 1, 1],
+      [6, 1],
+      [1, 9],
     ];
-    const max_number_positionArr = [
-      [0, 1],
-      [1, 3],
-    ];
-    expect(maxNumber(1, numberPanel, max_number_positionArr, "9")).toBe(98);
+    const max_number_positionArr = [[1, 1]];
+    expect(maxNumber(2, numberPanel, max_number_positionArr, "9")).toBe(96);
   });
   test("初回の最大数の位置かsら数字パネルをn-1回移動する時に通るマスを順番に並べた時の最大数を返す", () => {
     const numberPanel = [
@@ -34,7 +29,6 @@ describe("number box", () => {
       [1, 8, 1, 1],
     ];
     const max_number_positionArr = maxNumberPositionArr(numberPanel);
-    console.log(max_number_positionArr);
-    expect(maxNumber(3, numberPanel, max_number_positionArr, "9")).toBe(9786);
+    expect(maxNumber(4, numberPanel, max_number_positionArr, "9")).toBe(9786);
   });
 });
